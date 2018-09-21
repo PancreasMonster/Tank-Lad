4a4257474a124ea52746fe99fb6d5ba8e3677059
Ryan Flood <C16464342@mydit.ie>
2018-09-21 17:07:30 +0100

    Initial

---
+++ Assets/Scripts/Tank_Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank_Movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed = 20;
+    public float rotationSpeed = 20;
+    Vector3 tankOrientation;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(transform.up * rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-transform.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/WallSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawn : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int x = 0; x < 10; x++)
+        {
+          for (int y = 0; y < 5; y++)
+            {
+                GameObject clone;
+                clone = Instantiate(cube, new Vector3(0, y * 1.2f, -3 + x * 1.25f), Quaternion.identity);
+                clone.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            } 
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
